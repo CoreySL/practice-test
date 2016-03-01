108b57098433fff4ad91454d3a3ec9447bb0b950
CoreySL <cscottlin@gmail.com>
2016-03-01 13:17:43 -0800

    #1 I made a simple version of the search feature.

--- js/default.js
+++ js/default.js
@@ -4,78 +4,99 @@ var myRestaurants = [
     category: "steak",
     location: "Irvine, CA",
     price: "$$",
-    image: 'images/steak1.jpg'
+    image: 'images/steak1.jpg',
+    link: 'https://www.google.com'
   },
   {
     name: "Ruth's Chris Steak House",
     category: "steak",
     location: "Irvine, CA",
     price: "$$",
-    image: 'images/steak1.jpg'
+    image: 'images/steak1.jpg',
+    link: 'https://www.google.com'
   },
   {
     name: "Houston's Restaurant",
     category: "steak",
     location: "Irvine, CA",
     price: "$$",
-    image: 'images/steak1.jpg'
+    image: 'images/steak1.jpg',
+    link: 'https://www.google.com'
   },
   {
     name: "Outback Steakhouse",
     category: "steak",
     location: "Irvine, CA",
     price: "$$",
-    image: 'images/steak1.jpg'
+    image: 'images/steak1.jpg',
+    link: 'https://www.google.com'
   },
   {
     name: "Mastro's Steakhouse",
     cateogry: "steak",
     location: "Irvine, CA",
     price: "$$",
-    image: 'images/steak1.jpg'
+    image: 'images/steak1.jpg',
+    link: 'https://www.google.com'
   }
 ];
 
-var searchResults = getElementById('search-results');
+var searchResults = document.getElementById('results');
 
 function displayResults(array) {
   var resultsBox = document.createElement('div');
   resultsBox.className = "media panel panel-default";
+
   var imgBox = document.createElement('div');
   imgBox.className = "media-left media-middle";
+
   var restaurantImg = document.createElement('img');
   restaurantImg.className = "media-object";
-  restaurantImg.src= array.image;
+  restaurantImg.src = array.image;
   restaurantImg.setAttribute('width', '350px');
-  var reviewsLink = document.createElement('a');
-  reviewsLink.href="#";
+
+  var linkEl = document.createElement('a');
+  linkEl.title = "reviews link";
+  linkEl.href = "http://google.com";
+
   var bodyBox = document.createElement('div');
   bodyBox.className = "media-body";
-  var restaurantHeader = document.createElement('h4');
-  restaurantHeader.className = "media-heading";
 
+  var titleEl = document.createElement('h4');
+  titleEl.className = "media-heading";
   var restaurantTitle = document.createTextNode(array.name);
-  var restaurantInfo = document.createElement('p');
-  restaurantInfo.innerHTML = "category: " + array.category + "<br>" + "price: " + array.price + "<br>" + "location " + array.location;
 
-  searchResults.appendChild(resultsBox);
+  var catEl = document.createElement('p');
+  var restaurantCategory = document.createTextNode("category: " + array.category);
+
+  var locationEl = document.createElement('p');
+  var restaurantLocation = document.createTextNode('location: ' + array.location);
+
+  var priceEl = document.createElement('p');
+  var restaurantPrice = document.createTextNode('price: ' + array.price);
+
   resultsBox.appendChild(imgBox);
   resultsBox.appendChild(bodyBox);
-  bodyBox.appendChild(restaurantName);
-  restaurantImg.appendChild(reviewsLink);
-  restaurantName.appendChild(restaurantTitle);
-  bodyBox.appendChild(restaurantInfo);
+  bodyBox.appendChild(restaurantTitle);
+  imgBox.appendChild(restaurantImg);
+  titleEl.appendChild(restaurantTitle);
+  bodyBox.appendChild(titleEl);
+  catEl.appendChild(restaurantCategory);
+  bodyBox.appendChild(catEl);
+  locationEl.appendChild(restaurantLocation);
+  bodyBox.appendChild(locationEl);
+  priceEl.appendChild(restaurantPrice);
+  bodyBox.appendChild(priceEl);
+  bodyBox.appendChild(linkEl);
+  searchResults.appendChild(resultsBox);
 }
 
-var button = document.getElementById('search-button');
-console.log(button);
-
-function compareKeyword() {
-  var searchText = document.getElementById('search-text');
+function compareKeyword(e) {
+  var searchText = document.getElementById('keywords');
   searchText = searchText.value;
-  var restaurantName = myRestaurants[i].name
-  for (var i = 0; i < array.length; i++) {
-    if (array[i].[property].[value].tolowerCase() === searchText.tolowerCase()) {
+  for (var i = 0; i < myRestaurants.length; i++) {
+    console.log(myRestaurants[i].name + ' ' + searchText);
+    if (myRestaurants[i].name === searchText) {
       displayResults(myRestaurants[i]);
     }
     else {
@@ -85,6 +106,7 @@ function compareKeyword() {
       searchResults.appendChild.noResultHeader;
     }
   }
+}
 
-
-button.addEventListener('click',compareKeyword());
+var button = document.getElementById('search');
+button.addEventListener('click', compareKeyword);
