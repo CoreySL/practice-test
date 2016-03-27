0b0f4727b1089884a6cfb2dc4f63477147ccff05
CoreySL <cscottlin@gmail.com>
2016-03-27 16:23:51 -0700

    More changes to homelayout. Added top and bottom navs and played around with positinoning.

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,6 @@
 $(document).ready(function() {
   var $helpButton = $('.help-button');
   $helpButton.click(function() {
-      $('html, body').animate({ scrollTop: "850px"}, 1000);
+      $('html, body').animate({ scrollTop: "950px"}, 1000);
   });
 });
